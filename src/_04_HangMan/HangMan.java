4ca27685367a748e4190ac1fd280bbab5d24d481
sen_trix <44371257+sentrixd@users.noreply.github.com>
2020-05-05 19:52:13 -0700

    errr
    eeeee

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -17,10 +17,18 @@ public class HangMan implements KeyListener {
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
+	JLabel lives;
 	
 	String currentWord;
 	String word;
+	String pastCharacters;
+	
+	char[] characters;
+	
 	int wordIndex;
+	
+	int maxHealth = 10;
+	int health;
 	
 	public static void main(String[] args) {
 		new _04_HangMan.HangMan().run();
@@ -32,7 +40,7 @@ public class HangMan implements KeyListener {
 		
 		words = new Stack<String>();
 		
-		String input1 = JOptionPane.showInputDialog("Hangman: How many words would you like to guess? Please select a number below " + wordsInDictionary + "!");
+		String input1 = JOptionPane.showInputDialog("Hangman: How many words would you like to guess? Please select a number below " + wordsInDictionary + "! (Note: when you make sure to press enter after you are done)");
 		int convertedInput1 = Integer.parseInt(input1);
 		
 		for (int i = 0; i < convertedInput1; i ++) {
@@ -50,10 +58,14 @@ public class HangMan implements KeyListener {
 		currentWord = words.pop();
 		
 		word = "null";
+		health = 10;
+		
+		characters = new char[currentWord.length()];
 		
 		word = "";
 		for (int i = 0; i < currentWord.length(); i ++) {
 			word = word + "_";
+			characters[i] = '_';
 		}
 		
 		frame = new JFrame();
@@ -61,7 +73,10 @@ public class HangMan implements KeyListener {
 		frame.add(panel);
 		
 		label = new JLabel(word);
+		lives = new JLabel("Health: " + health + "/" + maxHealth);
+		
 		panel.add(label);
+		panel.add(lives);
 		
 		frame.pack();
 		frame.setVisible(true);
@@ -72,16 +87,105 @@ public class HangMan implements KeyListener {
 		
 		System.out.println(currentWord);
 	}
+	
+	void restartGame() {
+		currentWord = words.pop();
+		
+		word = "null";
+		health = 10;
+		
+		characters = new char[currentWord.length()];
+		
+		word = "";
+		for (int i = 0; i < currentWord.length(); i ++) {
+			word = word + "_";
+			characters[i] = '_';
+		}
+		
+		label.setText(word);
+		lives.setText("Health: " + health + "/" + maxHealth);
+		
+		frame.pack();
+		
+		wordIndex = 0;
+		
+		System.out.println(currentWord);
+	}
+	
+	void removeHealth() {
+		if (health > 0) {
+			health--;
+			lives.setText("Health: " + health + "/" + maxHealth);
+			
+			frame.pack();
+		} else {
+			JOptionPane.showMessageDialog(null, "Aw Man! You died.");
+			
+			restartGame();
+		}
+	}
+	
+	void checkIfWon() {
+		String wordConverted = makeStringFromCharArray(characters);
+		
+		if (wordConverted.contains(currentWord)) {
+			JOptionPane.showMessageDialog(null, "You won congrats!");
+			restartGame();
+		}
+	}
+	
+	String makeStringFromCharArray(char[] chr) {
+		String convertedString = "";
+		
+		for (int i = 0; i < chr.length; i++) {
+			convertedString = convertedString + chr[i];
+		}
+		
+		return convertedString;
+	}
 
 	@Override
 	public void keyTyped(KeyEvent ke) {
+		checkIfWon();
 		char keyChar = ke.getKeyChar();
-		String stringAtIndex = currentWord.substring(wordIndex, wordIndex + 1);
-		char charAtIndex = stringAtIndex.charAt(0);
+		boolean found = false;
 		
-		if (keyChar == charAtIndex) { 
-			System.out.println("yes");
+		for (int i = 0; i < currentWord.length(); i++) {
+			char charAtIndex = currentWord.charAt(i);
+			
+			if (keyChar == charAtIndex) {
+
+				characters[i] = charAtIndex;
+				
+				found = true;
+			}
 		}
+		
+		
+		if (!found) {
+			removeHealth();
+		}
+		
+		label.setText(makeStringFromCharArray(characters));
+		frame.pack();
+		
+		
+		//if (keyChar == charAtIndex) { 
+		//	System.out.println("yes");
+		//	word = currentWord.substring(wordIndex, wordIndex + 1) + word.substring(wordIndex + 1, word.length());
+			
+		//	wordIndex++;
+			
+		//	update();
+		//} else {
+		//	if (health <= 0) {
+		//		System.out.println("oof");
+				
+		//		JOptionPane.showMessageDialog(null, "You ran out of lives! Aw man!");
+		//	} else {
+		//		removeHealth();
+		//	}
+		//}
 	}
 
 	@Override
